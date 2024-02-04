dc0af7958d0f5a5455877e890e37ea4b76943887
moviefan322 <philman202@yahoo.com>
2024-02-04 10:10:58 -0500

    docker stuff all set up and working

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Carousel from "@/components/Carousel";
 import Greetings from "@/components/Greetings";
 import News from "@/components/News";
@@ -8,6 +8,24 @@ import Subscribe from "@/components/Subscribe";
 import Foot from "@/components/Foot";
 
 export default function Home() {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://localhost:3001/api/test");
+        if (!response.ok) {
+          console.log(response);
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json(); // Use .text() if the response is not JSON
+        console.log(data);
+      } catch (error) {
+        console.error("There was an error fetching the data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <>
       <Carousel />
--- server/README.md
+++
@@ -1,3 +0,0 @@
-MEVN template, deployment ready
-
-make sure to link the mongo and allow access before deployment
