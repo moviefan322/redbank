316c70d38a7ccb216def294e3a3e2868347a70fc
moviefan322 <philman202@yahoo.com>
2024-02-12 22:04:48 -0500

    styles imageUploader'

--- frontend/src/components/ImageUploader.tsx
+++ frontend/src/components/ImageUploader.tsx
@@ -9,6 +9,9 @@ import {
   selectImageUrl,
   selectError,
 } from "../features/upload/uploadSlice";
+import { FaCheckCircle } from "react-icons/fa";
+import { FaExclamationTriangle } from "react-icons/fa";
+import styles from "./ImageUploader.module.css";
 
 const ImageUploader: React.FC = () => {
   const [file, setFile] = useState<File | null>(null);
@@ -29,29 +32,41 @@ const ImageUploader: React.FC = () => {
   };
 
   return (
-    <div className="App">
-      <label htmlFor="file" className="btn-grey">
-        select file
-      </label>
-      {file && <center>{file?.name}</center>}
+    <div className="d-flex flex-row align-items-center py-2">
+      {file && <code>{file.name}</code>}
       <input
         id="file"
         type="file"
         onChange={handleSelectFile}
         multiple={false}
+        style={{ display: "none" }} // Inline style for hiding
       />
-      <code>
-        {imageUrl && (
-          <p className="output-item">
-            <span>Image URL:</span>
-            <span>{imageUrl}</span>
-          </p>
-        )}
-      </code>
-      {error && <p>Error: {error}</p>}
-      {file && (
-        <button onClick={handleUpload} className="btn-green">
-          {uploading ? "Uploading..." : "Upload to Cloudinary"}
+      {!file ? (
+        <button
+          onClick={() => document.getElementById("file")!.click()}
+          className="btn-admin mx-5"
+          style={imageUrl ? { display: "none" } : {}}
+        >
+          Choose File
+        </button>
+      ) : (
+        <button className="btn-admin-red mx-5" onClick={() => setFile(null)}>
+          Cancel
+        </button>
+      )}
+
+      {imageUrl && (
+        <>
+          {" "}
+          <FaCheckCircle className="me-3" size={20} /> Image Uploaded
+          Successfully
+        </>
+      )}
+
+      {error && <FaExclamationTriangle className={styles.error} size={20} />}
+      {file && !imageUrl && (
+        <button onClick={handleUpload} className="btn-admin">
+          {uploading ? "Uploading..." : "Upload"}
         </button>
       )}
     </div>
