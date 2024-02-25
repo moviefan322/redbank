c53bf8f365d27a6429a94cbdd3cda2c1bca94a5b
moviefan322 <philman202@yahoo.com>
2024-02-25 16:32:54 -0500

    styles allEvents page

--- frontend/src/pages/events/index.tsx
+++ frontend/src/pages/events/index.tsx
@@ -1,8 +1,25 @@
 import React from "react";
 import { useRouter } from "next/router";
 import Head from "next/head";
+import Link from "next/link";
 import { GetStaticProps } from "next";
 import Event from "@/types/Event";
+import styles from "./index.module.css";
+
+const months2 = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
 
 type Props = {
   events: Event[];
@@ -26,13 +43,45 @@ function EventsPage(props: Props): JSX.Element {
           content="Find a lot of great events that allow you to evolve..."
         />
       </Head>
-      <div>
+      <h1 className="text-center mt-5">Upcoming Events</h1>
+      <div className="d-flex flex-wrap align-items-center justify-content-center">
         {/* <EventSearch onSearch={findEventsHandler} /> */}
         {allEvents.map((event) => {
           return (
-            <div key={event._id}>
-              <h1>{event.title}</h1>
-              <p>{event.description}</p>
+            <div
+              key={event._id}
+              className="d-flex flex-column align-items-center"
+            >
+              <Link href={`/events/${event._id}`} className="nostyle-link">
+                <div
+                  className={`${styles.imageWrapper} m-3`}
+                  style={{
+                    backgroundColor: "#151515",
+                    backgroundImage: `url("${event.urlPhoto}")`,
+                    backgroundRepeat: "no-repeat",
+                    backgroundPosition: "center center",
+                    backgroundSize: "cover",
+                    width: "300px",
+                    height: "300px",
+                    position: "relative",
+                  }}
+                >
+                  <div className={styles.overlayText}>
+                    <h2>{event.title}</h2>
+                    <p>
+                      {months2[new Date(event.date).getMonth()]}{" "}
+                      {new Date(event.date).getDate()}
+                      {!event.allDay && (
+                        <>
+                          : {event.startTime} - {event.endTime}
+                        </>
+                      )}{" "}
+                      <br />
+                      {event.descriptionShort}
+                    </p>
+                  </div>
+                </div>
+              </Link>
             </div>
           );
         })}
