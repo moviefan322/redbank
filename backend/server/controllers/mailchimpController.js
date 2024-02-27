f1f7160de81a4dca5b64fd8116e621ee69b55ad0
moviefan322 <philman202@yahoo.com>
2024-02-27 12:33:12 -0500

    adds mailchimp controller

---
+++ backend/server/controllers/mailchimpController.js
@@ -0,0 +1,20 @@
+const axios = require('axios');
+
+const getMailchimpData = async (req, res) => {
+  try {
+    const url = "https://us7.api.mailchimp.com/3.0/campaigns?folder_id=c64693791f";
+    const apiKey = process.env.MAILCHIMP_API_KEY;
+    const response = await axios.get(url, {
+      auth: {
+        username: "anystring",
+        password: apiKey,
+      },
+    });
+    res.json(response.data);
+  } catch (error) {
+    console.error("Error fetching data from Mailchimp:", error);
+    res.status(500).send("Error fetching data from Mailchimp");
+  }
+};
+
+module.exports = { getMailchimpData };
