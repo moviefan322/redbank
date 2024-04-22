504ea32c8d4bc0f4516101140f5f477f64e3cf90
moviefan322 <philman202@yahoo.com>
2024-04-22 11:07:21 -0400

    adds route to add to mailing list

--- backend/server/routes/newsletterRoutes.js
+++ backend/server/routes/newsletterRoutes.js
@@ -3,11 +3,14 @@ const router = express.Router();
 import {
   getNewsletters,
   updateNewsletter,
+  addToMailingList,
 } from "../controllers/newsletterController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 router.route("/").get(getNewsletters);
 
 router.route("/:_id").put(protect, updateNewsletter);
+
+router.route("/add").post(addToMailingList);
 
 export default router;
