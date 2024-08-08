6dee7c53272267f3afeb0d567aa61484969ef343
moviefan322 <philman202@yahoo.com>
2024-08-08 14:51:11 -0400

    redirects to https

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/(.*)',
+        has: [
+          {
+            type: 'header',
+            key: 'x-forwarded-proto',
+            value: 'http',
+          },
+        ],
+        destination: 'https://redbank.org/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
