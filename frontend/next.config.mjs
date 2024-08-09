b04b095e6337397cc471c00d893ecfbfe5d0cb2a
moviefan322 <philman202@yahoo.com>
2024-08-09 15:37:36 -0400

    undo previous commit

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   async redirects() {
     return [
-      // Redirect HTTP to HTTPS for any domain
+      // Redirect HTTP to HTTPS for redbank.org
       {
         source: '/(.*)',
         has: [
@@ -12,8 +12,63 @@ const nextConfig = {
             key: 'x-forwarded-proto',
             value: 'http',
           },
+          {
+            type: 'host',
+            value: 'redbank.org',
+          },
         ],
-        destination: `https://${req.headers.host}/:path*`,
+        destination: 'https://redbank.org/:path*',
+        permanent: true,
+      },
+      // Redirect HTTP to HTTPS for www.redbank.org
+      {
+        source: '/(.*)',
+        has: [
+          {
+            type: 'header',
+            key: 'x-forwarded-proto',
+            value: 'http',
+          },
+          {
+            type: 'host',
+            value: 'www.redbank.org',
+          },
+        ],
+        destination: 'https://www.redbank.org/:path*',
+        permanent: true,
+      },
+      // Redirect HTTP to HTTPS for redbankoktoberfest.com
+      {
+        source: '/(.*)',
+        has: [
+          {
+            type: 'header',
+            key: 'x-forwarded-proto',
+            value: 'http',
+          },
+          {
+            type: 'host',
+            value: 'redbankoktoberfest.com',
+          },
+        ],
+        destination: 'https://redbankoktoberfest.com/:path*',
+        permanent: true,
+      },
+      // Redirect HTTP to HTTPS for www.redbankoktoberfest.com
+      {
+        source: '/(.*)',
+        has: [
+          {
+            type: 'header',
+            key: 'x-forwarded-proto',
+            value: 'http',
+          },
+          {
+            type: 'host',
+            value: 'www.redbankoktoberfest.com',
+          },
+        ],
+        destination: 'https://www.redbankoktoberfest.com/:path*',
         permanent: true,
       },
     ];
