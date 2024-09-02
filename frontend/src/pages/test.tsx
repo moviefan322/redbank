04cacbf91973c94efb69c2ef0947f33b6a0e6dac
moviefan322 <philman202@yahoo.com>
2024-09-02 18:01:35 -0400

    renders Test event at /test

--- frontend/src/pages/test.tsx
+++ frontend/src/pages/test.tsx
@@ -1,8 +1,104 @@
-import React from "react";
-import SearchComponent from "@/components/SearchComponent";
+import React, { Fragment, useState, useEffect } from "react";
+import Head from "next/head";
+import Link from "next/link";
+import DOMPurify from "dompurify";
+import { useAppDispatch } from "@/hooks/reduxHooks";
+import { getEvent } from "@/features/events/eventActions";
+import styles from "./test.module.css";
+import { formatTime } from "@/utils/formatTime";
+import Event  from "@/types/Event";
 
-const TestPoop = () => {
-  return <SearchComponent />;
-};
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 
-export default TestPoop;
+function Test() {
+  const dispatch = useAppDispatch();
+  const [event, setEvent] = useState<Event | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchEvent = async () => {
+      const result = await dispatch(getEvent('66d3ab437dc7fae6669f6581'));
+      if (getEvent.fulfilled.match(result)) {
+        setEvent(result.payload);
+      } else {
+        console.error(result.payload); // Handle errors appropriately
+      }
+      setLoading(false);
+    };
+
+    fetchEvent();
+  }, [dispatch]);
+
+  const sanitizeData = (data: string) => {
+    const sanitizedData = () => ({
+      __html: DOMPurify.sanitize(data),
+    });
+    return <div dangerouslySetInnerHTML={sanitizedData()} />;
+  };
+
+  if (!event) {
+    return (
+      <div className="center">
+        <h1>Loading..</h1>
+      </div>
+    );
+  }
+
+  return (
+    <Fragment>
+      <Head>
+        <title>{event.title}</title>
+        <meta name="description" content={event.description} />
+      </Head>
+      <div
+        className={`${styles.eventDetail} d-flex flex-column justify-content-center align-items-center mb-5`}
+      >
+        <Link
+          className="nostyle-link align-self-start mb-5 fw-bold"
+          href="/events"
+        >{`<< All Events`}</Link>
+        <h1 className="align-self-center">{event.title}</h1>
+        <h5>
+          {months[new Date(event.date).getMonth()]}{" "}
+          {new Date(event.date).getDate()}{" "}
+          {event.endDate
+            ? `- ${months[new Date(event.endDate).getMonth()]} ${new Date(
+                event.endDate
+              ).getDate()}`
+            : ""}
+          {!event.allDay! &&
+            `@ ${formatTime(event.startTime!)}-${formatTime(event.endTime!)}`}
+        </h5>
+        <div
+          className="m-3"
+          style={{
+            backgroundColor: "#151515",
+            backgroundImage: `url("${event.urlPhoto}")`,
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "center center",
+            backgroundSize: "cover",
+            width: "300px",
+            height: "300px",
+          }}
+        ></div>
+        <h6>{event.descriptionShort}</h6>
+        <div dangerouslySetInnerHTML={{ __html: event.description }} />
+      </div>
+    </Fragment>
+  );
+}
+
+export default Test;
